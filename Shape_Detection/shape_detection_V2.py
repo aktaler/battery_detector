252743b6e8dd97f1b5693b1c9139c4cc35ee57b9
FloBo <flo.boh@web.de>
2022-04-13 13:05:18 +0200

    Main

--- Shape_Detection/shape_detection_V2.py
+++ Shape_Detection/shape_detection_V2.py
@@ -1,3 +1,5 @@
+# Key "q" will break camera loop
+
 # import the necessary packages
 from shapedetector import ShapeDetector
 import imutils
@@ -31,15 +33,22 @@ while True:
             print("Can't receive frame")
             continue
 
-    # convert the image to grayscale, blur it slightly,
-    # threshold it and invert
+    # Image preprocessing gray->blurr->thresh->invert bitwise
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 2)
+    blurred = cv2.GaussianBlur(gray, (3, 3), 2)
+
+    # Sobel Edge Detection
+    # sobelxy = cv2.Sobel(src=blurred, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=3)  # Combined X and Y Sobel Edge Detection
+    # Canny Edge Detection
+    # canny = cv2.Canny(blurred, 10, 120)
+
     thresh = cv2.threshold(blurred, 110, 255, cv2.THRESH_BINARY)[1]
     inverted = cv2.bitwise_not(thresh)
 
     # show interim images
-    cv2.imshow("thresh", thresh)
+    # cv2.imshow("soblexy", sobelxy)
+    # cv2.imshow("Canny", canny)
+    # cv2.imshow("thresh", thresh)
     # cv2.imshow("blurred", blurred)
     cv2.imshow("inverted", inverted)
 
@@ -52,10 +61,10 @@ while True:
     # loop over the contours
     for c in cnts:
 
-        # Skip to small contours
-        if cv2.contourArea(c) < 2000:
+        # Skip over small and to large contours
+        if 2000 > cv2.contourArea(c) > 25000:
             continue
-
+        ''' not needed for now
         # compute the center of the contour
         M = cv2.moments(c)
         # Check if m00 has zero value and continue loop
@@ -64,23 +73,48 @@ while True:
             cY = int(M["m01"] / M["m00"])
         else:
             continue
+        '''
 
         # calculate and draw oriented rectangular box
+        # we use variable rect later to draw a gripper perimeter
         rect = cv2.minAreaRect(c)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
 
+        # Retrieve the key parameters of the rotated bounding box
+        center = (int(rect[0][0]), int(rect[0][1]))
+        width = int(rect[1][0])
+        height = int(rect[1][1])
+        angle = int(rect[2])
+
+        #swap height and width and draw gripper perimeter
+        if width > height:
+            rotated_box = (rect[0], (50, 120), rect[2])
+        else:
+            rotated_box = (rect[0], (120, 50), rect[2])
+
+        rotated_box = cv2.boxPoints(rotated_box)
+        rotated_box = np.int0(rotated_box)
+        cv2.drawContours(image, [rotated_box], 0, (127, 255, 0), 3)
+
+
+        #transform angle to +-90 degree system
+        if width < height:
+            angle = 90 - angle
+        else:
+           angle = -angle
+
+        # cv2.drawContours(image, gripper_peri, 0.5, (0, 255, 0), 2)
         # run shape classification on rectangular box
         shape = sd.detect(box)
 
-
         # draw everything on the image
         # cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-        string_list = ["Type"+shape, "CenterX="+str(cX), "CenterY="+str(cY), "alpha="+str(round(rect[2],2))]
-        y_Offset = cY
+        string_list = ["Type" + shape, "CenterX=" + str(box[0][0]), "CenterY=" + str(box[0][1]), "alpha=" + str(angle)]
+        y_Offset = box[0][1]
         for i in string_list:
-            cv2.putText(image, i, (cX, y_Offset), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv2.putText(image, i, (box[0][0], y_Offset), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
             y_Offset += 20
 
         # show the output image
